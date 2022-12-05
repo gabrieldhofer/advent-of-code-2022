913db1331cc2d0d22f430a624223747181e0c328
Gabriel Hofer <leirbagabriel468@gmail.com>
2022-12-05 10:10:30 -0600

    Update day4a.scala

--- day4a.scala
+++ day4a.scala
@@ -2,14 +2,16 @@ import scala.io.StdIn.readLine
 import scala.io.Source._
 import scala.collection.mutable
 
+
 def solve(lines: List[String]): Int = {
   val result = for x <- lines yield {
     val str = x.split(",")
     val r1: Array[Int] = str(0).split("-").map(x => x.toInt)
     val r2: Array[Int] = str(1).split("-").map(x => x.toInt)
+    val r1IntersectR2 = (r1(0) to r1(1)) intersect (r2(0) to r2(1))
 
-    if ((r1(0) to r1(1)) intersect (r2(0) to r2(1))) == (r1(0) to r1(1)) then 1
-    else if ((r1(0) to r1(1)) intersect (r2(0) to r2(1))) == (r2(0) to r2(1)) then 1
+    if r1IntersectR2 == (r1(0) to r1(1)) then 1
+    else if r1IntersectR2 == (r2(0) to r2(1)) then 1
     else 0
   }
   result.foldLeft(0)(_ + _)
