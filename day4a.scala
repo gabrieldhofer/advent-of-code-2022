ed44a7c98b4041c7ff160cd4277e50f7a4ea3f8f
Gabriel Hofer <leirbagabriel468@gmail.com>
2022-12-05 10:04:59 -0600

    Create day4a.scala

---
+++ day4a.scala
@@ -0,0 +1,23 @@
+import scala.io.StdIn.readLine
+import scala.io.Source._
+import scala.collection.mutable
+
+
+def solve(lines: List[String]): Int = {
+  val result = for x <- lines yield {
+    val str = x.split(",")
+    val r1: Array[Int] = str(0).split("-").map(x => x.toInt)
+    val r2: Array[Int] = str(1).split("-").map(x => x.toInt)
+
+    if ((r1(0) to r1(1)) intersect (r2(0) to r2(1))) == (r1(0) to r1(1)) then 1
+    else if ((r1(0) to r1(1)) intersect (r2(0) to r2(1))) == (r2(0) to r2(1)) then 1
+    else 0
+  }
+  result.foldLeft(0)(_ + _)
+}
+
+@main
+def main(): Unit = {
+  val lines = fromFile("src/main/scala/in.txt").getLines().toList
+  println(solve(lines))
+}
