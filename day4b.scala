84c326a44d275146522abd8d580056056eb4125d
Gabriel Hofer <leirbagabriel468@gmail.com>
2022-12-14 17:45:59 -0600

    Update day4b.scala

--- day4b.scala
+++ day4b.scala
@@ -2,7 +2,7 @@ import scala.io.StdIn.readLine
 import scala.io.Source._
 import scala.collection.mutable
 
-def solve(lines: List[String]): Int = {
+def solve(lines: List[String]): Int = 
   (for x <- lines yield
     val str = x.split(",")
     val r1: Array[Int] = str(0).split("-").map(x => x.toInt)
@@ -10,10 +10,8 @@ def solve(lines: List[String]): Int = {
     val r1IntersectR2: Seq[Int] = (r1(0) to r1(1)) intersect (r2(0) to r2(1))
     if r1IntersectR2.nonEmpty then 1 else 0
   ).foldLeft(0)(_ + _)
-}
 
 @main
-def main(): Unit = {
+def main(): Unit = 
   val lines = fromFile("src/main/scala/in.txt").getLines().toList
   println(solve(lines))
-}
