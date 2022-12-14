d18ab6e1ab02b2fbbfd516b7d426bac3463f1d0d
Gabriel Hofer <leirbagabriel468@gmail.com>
2022-12-14 17:49:21 -0600

    Update day3b.scala

--- day3b.scala
+++ day3b.scala
@@ -2,20 +2,15 @@ import scala.io.StdIn.readLine
 import scala.io.Source._
 import scala.collection.mutable
 
-def common(a: String, b: String): String = {
-  a intersect b
-}
-
 def solve(lines: List[String], prioritiesMap: scala.collection.mutable.Map[Char, Int]): Int = 
-  val result = for i <- 0 until lines.length by 3 yield {
+  (for i <- 0 until lines.length by 3 yield {
     val one: String = lines(i)
     val two: String = lines(i+1)
     val three: String = lines(i+2)
-    val four: String = common(one, two).toString()
-    val five: String = common(two, three).toString()
+    val four: String = (one intersect two).toString()
+    val five: String = (two intersect three).toString()
     prioritiesMap(common(four, five).toString().charAt(0))
-  }
-  result.foldLeft(0)(_ + _)
+  }).foldLeft(0)(_ + _)
 
 @main
 def main(): Unit = 
