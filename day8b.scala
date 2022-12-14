600bdbdcdc25946be6f0e2b1a3170ced6e2c3f1a
Gabriel Hofer <leirbagabriel468@gmail.com>
2022-12-14 08:45:05 -0600

    Update day8b.scala

--- day8b.scala
+++ day8b.scala
@@ -5,19 +5,16 @@ def left(lines: Array[String], x: Int, y: Int, h: Int, w: Int): Int =
   y
 
 def right(lines: Array[String], x: Int, y: Int, h: Int, w: Int): Int =
-  // !(for i <- y+1 until w yield lines(x)(y).toInt <= lines(x)(i).toInt).contains(true)
   for i <- y+1 until w do
     if lines(x)(y).toInt <= lines(x)(i).toInt then return i - y
   w - y - 1
 
 def up(lines: Array[String], x: Int, y: Int, h: Int, w: Int): Int =
-  //!(for i <- 0 until x yield lines(x)(y).toInt <= lines(i)(y).toInt).contains(true)
   for i <- x-1 to 0 by -1 do
     if lines(x)(y).toInt <= lines(i)(y).toInt then return x - i
   x
 
 def down(lines: Array[String], x: Int, y: Int, h: Int, w: Int): Int =
-  //!(for i <- x+1 until h yield lines(x)(y).toInt <= lines(i)(y).toInt).contains(true)
   for i <- x+1 until h do
     if lines(x)(y).toInt <= lines(i)(y).toInt then return i - x
   h - x - 1
@@ -37,6 +34,5 @@ def main(): Unit =
   val h = lines.length
   val w = lines(0).length
   val part2 = solve(lines, h, w)
-  println(part2)
   val part3 = part2.max
   println("part 3: " + part3)
